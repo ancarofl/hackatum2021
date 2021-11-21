727e1529b98803484aea79afc459e02efdcc5e33
Anca N <negulescuanca11@gmail.com>
2021-11-21 01:58:03 +0100

    Move vars from index to sim and show cars on map after retrieving them.

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// For testing assign lat/lng pairs that we know work so we don't have to fill in the 'form'
-// Otherwise we get a 400 Malformed request, cause: 'Error while parsing request: \n\n\tInvalid location specification at ','\n\tInvalid
-// waypoint at ','\n\tUnexpected input at ','\n\tInvalid value for parameter 'origin' at ',''. The form should later be changed to work with
-// address instead of lat/lng.
-
-// TUM coords
-var olat = MUNICH_TUM_LAT;
-var olng = MUNICH_TUM_LNG;
-
-// UvA coords
-// var dlat = 52.35632784585438;
-// var dlng = 4.971252886117597;
-
-// Munich Airport coords
-var dlat = MUNICH_AIRPORT_LAT;
-var dlng = MUNICH_AIRPORT_LNG;
-
-// For reference
-// Using HERE
-// https://wego.here.com/directions/mix/Munich-Airport,-Terminalstra%C3%9Fe-West,-85356-Oberding:276u287h-33978e49adeb4b2fa026780cb8d88bef/Technische-Universit%C3%A4t-M%C3%BCnchen,-Arcisstra%C3%9Fe-21,-80333-Munich:276u281z-68ca4dc5f40c475e8f5d5793dcec32b9?map=48.25352,11.67727,12,normal
-// Using Google
-// https://www.google.com/maps/dir/Munich+Airport,+M%C3%BCnchen-Flughafen,+Germany/Technische+Universit%C3%A4t+M%C3%BCnchen,+Arcisstra%C3%9Fe+21,+80333+M%C3%BCnchen,+Germany/@48.2531166,11.5341618,11z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x479e13dfa898f107:0xcb3789d757b96c43!2m2!1d11.7862396!2d48.3527322!1m5!1m1!1s0x479e7261336d8c11:0x79a04d44dc5bf19d!2m2!1d11.5678602!2d48.14966!3e0
-
-var cars = null;
-var bookings = null;
-
-var timesToPassengerArray = [];
-var localBookingsArray = [];
-
-/*function callTaxi() {
-	console.log("Call taxi!");
-}*/
-
-
-
 
 
 
